e5623b852c47d25bf5d6c065e44df5ffb94b169c
Televerso <dbogat23@yandex.ru>
2022-06-01 23:55:18 +0400

    1.13

--- Menu2/Minesweeper.cs
+++ Menu2/Minesweeper.cs
@@ -9,7 +9,9 @@ namespace Menu2
   {
     private MinesweeperBoard board;
     private Button[,] allButtons;
-    private int scale = 35;
+    private Button exit;
+    private Button reset;
+    private int scale;
 
     private int level;
     
@@ -21,37 +23,101 @@ namespace Menu2
 
     private void Form1_Load(object sender, EventArgs e)
     {
+      BackgroundImage = Image.FromFile("Back7.png");
+      BackgroundImageLayout = ImageLayout.Stretch;
+      SizeChanged += new EventHandler(size_Change);
+      
       int[] diff = MinesweeperLevelLayout.GetLevel(level);
       board = new MinesweeperBoard(diff[0],diff[1]);
       board.RandomFillBoard(diff[2]);
-      allButtons = new Button[diff[0],diff[1]];
-      for (int i = 0; i != board.GetXSide(); ++i)
+      
+      scale = (Math.Min(Height - 50, Width - 30)) / Math.Max(board.GetXSide()+4,board.GetYSide()+2);
+      if (scale < 10) scale = 10;
+
+      exit = new Button();
+      exit.TabIndex = 2000;
+      exit.Size = new Size((int) (scale*4),scale*2);
+      exit.Location = new Point(10, 10);
+      exit.Text = "Назад";
+      exit.FlatStyle = FlatStyle.Flat;
+      exit.BackColor = Color.FromArgb(255, 240, 226, 204);
+      exit.ForeColor = Color.FromArgb(255, 95, 45, 27);
+      exit.Font = new Font("Bahnschrift Light", 12, FontStyle.Bold);
+      exit.MouseClick += new MouseEventHandler(exit_Click);
+      Controls.Add(exit);
+      
+      reset = new Button();
+      reset.TabIndex = 1999;
+      reset.Size = new Size((int) (scale*4),scale*2);
+      reset.Location = new Point(10, 10+scale*2);
+      reset.Text = "Заново";
+      reset.FlatStyle = FlatStyle.Flat;
+      reset.BackColor = Color.FromArgb(255, 240, 226, 204);
+      reset.ForeColor = Color.FromArgb(255, 95, 45, 27);
+      reset.Font = new Font("Bahnschrift Light", 12, FontStyle.Bold);
+      reset.MouseClick += new MouseEventHandler(reset_Click);
+      Controls.Add(reset);
+
+      allButtons = new Button[diff[0], diff[1]];
+      int[] offset = {scale * 4, scale*2};
+      
+      for(int i = 0; i != board.GetXSide(); ++i)
       {
         for (int j = 0; j != board.GetYSide(); ++j)
         {
           Button button = new Button();
-          button.Location = new Point(10+scale*j, 10+scale*i);
+          button.Location = new Point(offset[0]+10+scale*j, offset[1]+10+scale*i);
           button.Size = new Size(scale, scale);
           button.Text = board.GetRealBoard()![i,j].ToString();
+          button.TabIndex = i * board.GetXSide() + j;
+          button.FlatStyle = FlatStyle.Flat;
           allButtons[j, i] = button;
           Controls.Add(button);
           button.MouseDown += new MouseEventHandler(button1_Click);
         }
       }
+    }
 
-      Button resetButton = new Button();
-      resetButton.Location = new Point(10+scale * 2, 30 + scale * 8);
-      resetButton.Size = new Size(scale*2, scale);
-      resetButton.Text = "Reset";
-      Controls.Add(resetButton);
-      resetButton.Click += new EventHandler(button2_Click);
-        
-      Button genre = new Button();
-      genre.Location = new Point(10+scale * 4, 30 + scale * 8);
-      genre.Size = new Size(scale*2, scale);
-      genre.Text = "Genre choice";
-      Controls.Add(genre);
-      genre.Click += new EventHandler(button3_Click);
+    private void size_Change(object sender, EventArgs e)
+    {
+      scale = (Math.Min(Height - 50, Width - 30)) / Math.Max(board.GetXSide()+4,board.GetYSide()+2);
+      if (scale < 10) scale = 10;
+      int[] offset = {scale * 4, scale*2};
+      
+      exit.Size = new Size((int) (scale*4),scale*2);
+      reset.Size = new Size((int) (scale*4),scale*2);
+      reset.Location = new Point(10, 10+scale*2);
+      
+      for(int i = 0; i != board.GetXSide(); ++i)
+      {
+        for (int j = 0; j != board.GetYSide(); ++j)
+        {
+          allButtons[j,i].Location = new Point(offset[0]+10+scale*j, offset[1]+10+scale*i);
+          allButtons[j,i].Size = new Size(scale, scale);
+        }
+      }
+    }
+
+    private void reset_Click(object sender, MouseEventArgs e)
+    {
+      int[] diff = MinesweeperLevelLayout.GetLevel(level);
+      board = new MinesweeperBoard(diff[0],diff[1]);
+      board.RandomFillBoard(diff[2]);
+      
+      for (int i = 0; i != board.GetXSide(); ++i)
+      {
+        for (int j = 0; j != board.GetYSide(); ++j)
+        {
+          allButtons[i, j].Text = board.GetRealBoard()![i, j].ToString();
+        }
+      }
+    }
+
+    private void exit_Click(object sender, MouseEventArgs e)
+    {
+      this.Close();
+      LevelSelectorTele levelSelector = new LevelSelectorTele('M', 10);
+      levelSelector.Show();
     }
 
     private void button1_Click(object sender, MouseEventArgs e)
@@ -90,33 +156,5 @@ namespace Menu2
         }
       }
     }
-
-    private void button2_Click(object sender, EventArgs e)
-    {
-      Button senderB = (Button) sender;
-      board.RecreateBoard(8);
-      board.RandomFillBoard(level);
-      for (int i = 0; i != board.GetXSide(); ++i)
-      {
-        for (int j = 0; j != board.GetYSide(); ++j)
-        {
-          allButtons[i, j].Text = board.GetRealBoard()![i, j].ToString();
-        }
-      }
-    }
-
-    private void button3_Click(object sender, EventArgs e)
-    {
-      this.Hide();
-      GenreChoice genre = new GenreChoice();
-      genre.Show();
-
-    }
-    //private void ReturnToLevels(object sender, EventArgs e)
-    //{
-   //   this.Hide(); 
-    //  GenreChoice second = new GenreChoice();
-   //   second.Show();
-  //  }
   }
 }
