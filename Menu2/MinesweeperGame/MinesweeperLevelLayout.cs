529f8e10d84f91ac0aee608fe5eeb0b72bb55050
Богатырев Дмитрий Владимирович <101474789+Televerso@users.noreply.github.com>
2022-06-02 09:47:18 +0400

    Update MinesweeperLevelLayout.cs
    11

--- Menu2/MinesweeperGame/MinesweeperLevelLayout.cs
+++ Menu2/MinesweeperGame/MinesweeperLevelLayout.cs
@@ -4,7 +4,7 @@
     {
         private static readonly int[] SideX = new int[]
         {
-            8,10,20,10,16,18,20,24,30,36,1
+            8,10,10,10,16,18,20,24,30,36,1
         };
         private static readonly int[] SideY = new int[]
         {
@@ -12,7 +12,7 @@
         };
         private static readonly int[] Count = new int[]
         {
-            3,6,13,14,30,40,70,100,130,160,1
+            3,6,10,14,30,40,70,100,130,160,1
         };
 
         public static int[] GetLevel(int lvl)
